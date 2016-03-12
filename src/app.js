3abc0b965d4481e57f665e5df7e402e5b1421efd
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2016-03-13 01:20:37 +0200

    added support for personal onenote.com notebooks

--- src/app.js
+++ src/app.js
@@ -3,19 +3,21 @@ var ajax = require('ajax');
 var Settings = require('settings');
 
 var appUrl = 'http://markeev.com/pebble/onenote.html';
-var auth_code = Settings.option('auth_code');
-var clientId='127482e0-2e1a-4e80-9b81-8a13f9fb822c'; // Azure app id
-var clientSecret=''; // Azure app secret
+
+var auth_url = '';
+var baseApiUrl = '';
+var clientId='';
+var clientSecret='';
 
 Settings.config({ url: appUrl }, function() {
-    auth_code = Settings.option('auth_code');
     localStorage.removeItem('refresh_token');
+    console.log('Settings closed!');
     retrieveNotebooks();
 });
 
-if (!auth_code)
+if (!Settings.option('auth_code'))
 {
-    errorCallback('Account is not configured. Please visit settings.');
+    showText('Account is not configured. Please visit settings.');
     Pebble.openURL(appUrl);
     return;
 }
@@ -24,15 +26,32 @@ else
 
 function retrieveNotebooks()
 {
+  var account_type = Settings.option('account_type');
+    
+  if (account_type == 'onenote.com') {
+      clientId = '000000004818A9A6';
+      clientSecret = '';
+      auth_url = 'https://login.live.com/oauth20_token.srf';
+      baseApiUrl = 'https://www.onenote.com/api/v1.0/';
+  } else {
+      clientId='127482e0-2e1a-4e80-9b81-8a13f9fb822c'; // Azure app id
+      clientSecret=''; // Azure app secret
+      auth_url = 'https://login.microsoftonline.com/common/oauth2/token?api-version=beta';
+      baseApiUrl = 'https://graph.microsoft.com/beta/';
+  }
+
   if (localStorage.getItem('refresh_token') !== null)
     authWithRefreshToken();
   else
-    authWithAccessCode();    
+    authWithAccessCode();
    
 }
 
 function authWithRefreshToken()
 {
+    console.log('Auth with refresh token');
+
+
     ajax({
             url: 'https://login.microsoftonline.com/common/oauth2/token?api-version=beta',
             method: 'POST',
@@ -45,184 +64,105 @@ function authWithRefreshToken()
             }
         },
         authorizedCallback,
-        errorCallback
+        showError
     );
 }
 
 function authWithAccessCode()
 {
+    console.log('Auth with access code');
+
     ajax({
-            url: 'https://login.microsoftonline.com/common/oauth2/token?api-version=beta',
+            url: auth_url,
             method: 'POST',
             data: {
                 grant_type: 'authorization_code',
-                code: auth_code,
+                code: Settings.option('auth_code'),
                 redirect_uri: appUrl,
                 client_id: clientId,
                 client_secret: clientSecret
             }
         },
         authorizedCallback,
-        errorCallback
+        showError
     );
 }
 
 var access_token = '';
 function authorizedCallback(data)
 {
+    console.log("returned: " + data);
     var dataObj = JSON.parse(data);
     access_token = dataObj.access_token;
     var refresh_token = dataObj.refresh_token;
     localStorage.setItem('refresh_token', refresh_token);
-    console.log("returned: " + data);
     
     // get list of notebooks
     ajax(
       {
-        url: 'https://graph.microsoft.com/beta/me/notes/notebooks',
-        headers: { 
-          "Authorization": "Bearer " + access_token
-        }
+        url: baseApiUrl + '/me/notes/notebooks',
+        headers: { "Authorization": "Bearer " + access_token }
       },
-      notebooksRequestSuccess,
-      errorCallback
+      function(data) {
+        showMenu('Notebooks', data, 'name', notebookSelected);
+      },
+      showError
     );
 }
 
 var notebook_id = '';
 var notebook_title = '';
-function notebooksRequestSuccess(data)
+function notebookSelected(e, id)
 {
-    console.log("notebooks data: " + data);
-    var dataObj = JSON.parse(data);
+    notebook_id = id;
+    notebook_title = e.item.title;
 
-    var notebooks_menu = [];
-    var notebooks_ids = [];
-    for (var i=0;i<dataObj.value.length;i++)
-    {
-        notebooks_menu.push({
-            title: dataObj.value[i].name
-        });
-        
-        notebooks_ids.push(dataObj.value[i].id);
-    }
-    var menu = new UI.Menu({
-        sections: [
-            {
-                title: 'Notebooks',
-                items: notebooks_menu
-            }
-        ]
-    });
-    
-    menu.on('select', function(e) {
-        notebook_id = notebooks_ids[e.itemIndex];
-        notebook_title = e.item.title;
-        
-        // get sections of the selected notebook
-        ajax(
-          {
-            url: 'https://graph.microsoft.com/beta/me/notes/notebooks/' + notebook_id + '/sections',
-            headers: { 
-              "Authorization": "Bearer " + access_token
-            }
-          },
-          sectionsRequestSuccess,
-          errorCallback
-        );
-    });
-        
-    menu.show();
+    // get sections of the selected notebook
+    ajax(
+        {
+            url: baseApiUrl + '/me/notes/notebooks/' + notebook_id + '/sections',
+            headers: { "Authorization": "Bearer " + access_token }
+        },
+        function(data) {
+          showMenu('Sections', data, 'name', sectionSelected);
+        },
+        showError
+    );
 }
 
 var section_id = '';
-function sectionsRequestSuccess(data)
+function sectionSelected(e, id)
 {
-    console.log("sections data: " + data);
-    var dataObj = JSON.parse(data);
-    var sections_menu = [];
-    var sections_ids = [];
-    for (var i=0;i<dataObj.value.length;i++)
-    {
-        sections_menu.push({
-            title: dataObj.value[i].name
-        });
-        
-        sections_ids.push(dataObj.value[i].id);
-    }
-    var menu = new UI.Menu({
-        sections: [
-            {
-                title: 'Sections of ' + notebook_title,
-                items: sections_menu
-            }
-        ]
-    });
-    
-    menu.on('select', function(e) {
-        section_id = sections_ids[e.itemIndex];
-        
-        // get pages of the section
-        ajax(
-          {
-            url: 'https://graph.microsoft.com/beta/me/notes/sections/' + section_id + '/pages',
-            headers: { 
-              "Authorization": "Bearer " + access_token
-            }
-          },
-          pagesRequestSuccess,
-          errorCallback
-        );
-    });
-        
-    menu.show();
+    section_id = id;
+
+    // get pages of the section
+    ajax(
+        {
+            url: baseApiUrl + '/me/notes/sections/' + section_id + '/pages',
+            headers: { "Authorization": "Bearer " + access_token }
+        },
+        function(data) {
+          showMenu('Pages', data, 'title', pageSelected);
+        },
+        showError
+    );
     
 }
 
 var page_id = '';
-function pagesRequestSuccess(data)
+function pageSelected(e, id)
 {
-    console.log("pages data: " + data);
-    var dataObj = JSON.parse(data);
-    var pages_menu = [];
-    var pages_ids = [];
-    for (var i=0;i<dataObj.value.length;i++)
-    {
-        if (!dataObj.value[i].title)
-            continue;
-        
-        pages_menu.push({
-            title: dataObj.value[i].title
-        });
-        
-        pages_ids.push(dataObj.value[i].id);
-    }
-    var menu2 = new UI.Menu({
-        sections: [
-            {
-                title: 'Pages',
-                items: pages_menu
-            }
-        ]
-    });
+    page_id = id;
 
-    menu2.on('select', function(e) {
-        page_id = pages_ids[e.itemIndex];
-        
-        // get contents of the page
-        ajax(
-          {
-            url: 'https://graph.microsoft.com/beta/me/notes/pages/' + page_id + '/content',
-            headers: { 
-              "Authorization": "Bearer " + access_token
-            }
-          },
-          pageContentRequestSuccess,
-          errorCallback
-        );
-    });
-        
-    menu2.show();
+    // get contents of the page
+    ajax(
+        {
+            url: baseApiUrl + '/beta/me/notes/pages/' + page_id + '/content',
+            headers: { "Authorization": "Bearer " + access_token }
+        },
+        pageContentRequestSuccess,
+        showError
+    );
     
 }
 
@@ -242,7 +182,40 @@ function pageContentRequestSuccess(data)
 
 }
 
-function errorCallback()
+function showMenu(title, data, title_property, select_callback)
+{
+    console.log("data received: " + data);
+    var dataObj = JSON.parse(data);
+    
+    if (dataObj.value.length === 0) {
+        showText(title + ' not found.');
+        return;
+    }
+
+    var menuitems = [];
+    var ids = [];
+    for (var i=0;i<dataObj.value.length;i++)
+    {
+        menuitems.push({
+            title: dataObj.value[i][title_property] || '(untitled)'
+        });
+        
+        ids.push(dataObj.value[i].id);
+    }
+    var menu = new UI.Menu({
+        sections: [
+            {
+                title: title,
+                items: menuitems
+            }
+        ]
+    });
+    
+    menu.on('select', function(e) { select_callback(e, ids[e.itemIndex]); });
+    menu.show();
+}
+
+function showError()
 {
     var main = new UI.Card({
         title: 'Error',
@@ -250,4 +223,14 @@ function errorCallback()
         scrollable: true
     });
     main.show();
+}
+
+function showText(text)
+{
+    var main = new UI.Card({
+        body: text,
+        scrollable: true
+    });
+    main.show();
+    
 }
