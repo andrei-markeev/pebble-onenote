5199a6218972031284b26b03ee2054e53334a9c9
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2016-07-06 17:02:58 +0300

    added simplified Round support

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ var UI = require('ui');
 var ajax = require('ajax');
 var Settings = require('settings');
 var Vector2 = require('vector2');
+var Feature = require('platform/feature');
 
 var appUrl = 'http://markeev.com/pebble/onenote.html';
 
@@ -92,7 +93,6 @@ function authWithAccessCode()
 var access_token = '';
 function authorizedCallback(data)
 {
-    console.log("returned: " + data);
     var dataObj = JSON.parse(data);
     access_token = dataObj.access_token;
     var refresh_token = dataObj.refresh_token;
@@ -172,70 +172,140 @@ function pageContentRequestSuccess(data)
     var pageFontSize = Settings.option('font_size') || 24;
     
     console.log(data);
+    
     var title;
     var text = data.replace(/<title>([^<]*)<\/title>/, function(m) { title = m.replace(/<(?:.|\n)*?>/gm, ''); return ''; });
     
-    var listItems = [];
-    text = text.replace(/<li>(.*?)<\/li>/g, function(m) {
-        listItems.push({ 
-            text: decodeHTMLEntities(m.replace(/<(?:.|\n)*?>/gm, '').replace(/\s+/g, ' ')),
-            tag: m.match(/data\-tag="([A-Za-z,:\-]+)"/)[1]
-        });
-        return '[listItem]';
-    });
-
-    console.log(JSON.stringify(listItems));
+    if (Feature.round()) {
+        console.log(htmlToText(text));
+        new UI.Card({
+            title: title,
+            body: htmlToText(text),
+            scrollable: true
+        }).show();
+        return;
+    }
     
-    text = text.replace(/<(br \/|p|\/p|li)>/gm, '[br]');
-    text = text.replace(/<(?:.|\n)*?>/gm, '').replace(/\s+/g, ' ').replace(/\[br\]/g,'\n');
-    text = decodeHTMLEntities(text);
-
-    var page = new UI.Window({
-      backgroundColor: 'white',
-      scrollable: true
-    });
-    
-    var yPos = 0;
-    var titleSize = sizeVector(title, pageFontSize);
-    var titleText = new UI.Text({ position: new Vector2(0, yPos), size: titleSize, text: title, textAlign: 'center', textOverflow: 'wrap', color: 'black', font: 'gothic-' + pageFontSize + '-bold' });
-    page.add(titleText);
-    yPos += titleSize.y + 5;
-
-    var texts = text.split('[listItem]');
-    while (texts.length > 0)
-    {
-        var fragmentText = texts.shift();
-        
-        if (fragmentText.replace(/[\s\n]+/g,'') !== '') {
-            var fragmentSize = sizeVector(fragmentText, pageFontSize);
-            var fragmentElement = new UI.Text({ position: new Vector2(4, yPos), size: fragmentSize, text: fragmentText, textAlign: 'left', textOverflow: 'wrap', color: 'black', font: 'gothic-' + pageFontSize });
-            page.add(fragmentElement);
-            yPos += fragmentSize.y + 2;
+    var items = [];
+    var listTypes = [];
+    var listCounters = [];
+    var openedListItems = [];
+    var counter = 0;
+    text = '<li>' + text + '</li>';
+    text = text.replace(/<\/?[uo]l>|<li[^>]*>(?:<span[^>]*>)?|<\/li>/gm, function(m, offset) {
+        if (m == '<ul>' || m == '<ol>') {
+            listTypes.push(m);
+            listCounters.push(counter);
+            counter = 0;
+            return m;
         }
-
-        if (listItems.length > 0) {
-            var listItem = listItems.shift();
-            var isTodo = listItem.tag.indexOf('to-do') != -1;
-            if (isTodo) {
-                page.add(new UI.Rect({ position: new Vector2(2, yPos + 4), size: new Vector2(16, 16), borderColor: 'black' }));
-                page.add(new UI.Rect({ position: new Vector2(3, yPos + 5), size: new Vector2(14, 14), borderColor: 'black' }));
-                if (listItem.tag.indexOf('to-do:completed') != -1) {
-                    console.log('adding the checkmark image');
-                    page.add(new UI.Image({ position: new Vector2(5, yPos + 7), size: new Vector2(11, 11), image: 'images/check.bmp' }));
+        if (m == '</ul>' || m == '</ol>') {
+            listTypes.pop();
+            counter = listCounters.pop();
+            return m;
+        }
+        if (m == '</li>') {
+            var closingLi = openedListItems.pop();
+            closingLi.endOffset = offset;
+            closingLi.text = text.slice(closingLi.startOffset, offset);
+            
+            for (var i=items.length-1;i>=0;i--) {
+                var li = items[i];
+                if (li.level == closingLi.level+1 && li.startOffset > closingLi.startOffset)
+                {
+                    closingLi.children.unshift(li);
+                    var start = li.startOffset - closingLi.startOffset;
+                    var end = li.endOffset - closingLi.startOffset;
+                    console.log(closingLi.text.slice(start,end));
+                    closingLi.text = closingLi.text.substr(0, start) + '[---childItem---]' + closingLi.text.substr(end);
                 }
             }
-            var listItemSize = sizeVector(listItem.text, pageFontSize, isTodo ? 20 : 0);
-            var listItemElement = new UI.Text({ position: new Vector2(isTodo ? 24 : 4, yPos), size: listItemSize, text: listItem.text, textAlign: 'left', textOverflow: 'wrap', color: 'black', font: 'gothic-' + pageFontSize });
-            page.add(listItemElement);
-            yPos += listItemSize.y + 2;
+            
+            closingLi.text = htmlToText(closingLi.text);
+            return m;
         }
-    }
+
+        counter++;
+        var counterMatch = m.match(/^<li value="([A-Za-z0-9]+)"/);
+        if (counterMatch && counterMatch.length > 1 && +counterMatch[1])
+            counter = +counterMatch[1];
+
+        var tagMatch = m.match(/data\-tag="([A-Za-z,:\-]+)"/);
+        var tag = tagMatch && tagMatch.length > 1 ? tagMatch[1] : '';
+        if (tag === '' && listTypes.length > 0)
+            tag = listTypes[listTypes.length - 1];
+        var newListItem = {
+            tag: tag,
+            level: listTypes.length,
+            counter: counter,
+            startOffset: offset + m.indexOf('>') + 1,
+            children: []
+        };
+        items.push(newListItem);
+        openedListItems.push(newListItem);
+        return m;
+    });
+        
+    var page = new UI.Window({
+      backgroundColor: 'white',
+      scrollable: true,
+      
+    });
+    
+    var yPos = addTextToWindow(page, 0, 0, title, pageFontSize) + 2;
+    console.log(JSON.stringify(items[0], null, 4));
+    showItem(items[0], page, pageFontSize, yPos);
     
     page.show();
 }
 
+function showItem(item, page, pageFontSize, yPos)
+{
+    var texts = item.text.split('[---childItem---]');
+    while (texts.length > 0)
+    {
+        var padding = item.level * 4 + 2;
+        var fragmentText = texts.shift();
+        
+        if (fragmentText.replace(/[\s\n]+/g,'') !== '') {
+            yPos = addTextToWindow(page, (item.level === 0 ? 0 : 20 ) + padding, yPos, fragmentText, pageFontSize);
+        }
+
+        if (item.children.length > 0) {
+            var childItem = item.children.shift();
+            if (childItem.tag.indexOf('to-do') != -1) {
+                page.add(new UI.Rect({ position: new Vector2(padding, yPos + 4 + (pageFontSize - 18)), size: new Vector2(16, 16), borderColor: 'black' }));
+                page.add(new UI.Rect({ position: new Vector2(padding + 1, yPos + 5 + (pageFontSize - 18)), size: new Vector2(14, 14), borderColor: 'black' }));
+                if (childItem.tag.indexOf('to-do:completed') != -1)
+                    page.add(new UI.Image({ position: new Vector2(padding + 3, yPos + 7 + (pageFontSize - 18)), size: new Vector2(11, 11), image: 'images/check.bmp' }));
+            }
+            else if (childItem.tag == '<ul>')
+                page.add(new UI.Circle({ position: new Vector2(padding + 6, yPos + 10 + (pageFontSize - 18)), radius: 4, borderColor: 'black', backgroundColor: 'black' }));
+            else if (childItem.tag == '<ol>')
+                page.add(new UI.Text({ position: new Vector2(padding, yPos + (pageFontSize - 18)), size: new Vector2(18, 18), text: childItem.counter + '.', color: 'black', font: 'gothic-18' }));
+            
+            yPos = showItem(childItem, page, pageFontSize, yPos);
+        }
+    }
+    
+    return yPos;
+    
+}
+
+function htmlToText(text)
+{
+    text = text.replace(/<!\-\- OutlineGroupNode is not supported \-\->/g, '*** Error: fragment missing (not supported by API). Try adding some text before this node. *** [---br---]');
+    text = text.replace(/<(br \/|p[^>]*|\/p|td[^>]*|tr|li[^>]*)>/g, '[---br---]');
+    text = text.replace(/<[\s\S]*?>/g, ' ').replace(/\s+/g, ' ').replace(/\[\-\-\-br\-\-\-\]/g,'\n');
+    text = text.replace(/[\s\r]*\n[\s\r]*\n[\s\r\n]*/, '\n\n').replace(/[\s\r]*\n[\s\r\n]*/, '\n');
+    text = text.replace(/^[\s\r\n]+/m,'').replace(/[\s\r\n]+$/m,'').replace(/[\s\r\n]+\[\-\-\-childItem\-\-\-\]/m,'');
+    text = decodeHTMLEntities(text);
+    return text;
+}
+
 function decodeHTMLEntities(text) {
     var entities = [
+        ['quot', '"'],
         ['apos', '\''],
         ['amp', '&'],
         ['lt', '<'],
@@ -306,42 +376,137 @@ function showText(text)
     
 }
 
-function strTruncate(string, width) {
-    if (string.length >= width) {
-        var result = string[width - 1] === ' ' ? string.substr(0, width - 1) : string.substr(0, string.substr(0, width).lastIndexOf(' '));
-        if (result.length === 0)
-            result = string.substr(0, width - 1);
+function getCharWidth(ch, fontSize) {
+    
+    if (fontSize == 18) {
+        if (/[\(\)t]/.test(ch))
+            return 5;
+        else if (/[jiIJl1:;\.,]/.test(ch))
+            return 4;
+        else if (/[ABCDEFHGKLNOPQRSTUVXYZАБВГДЕЁЗИЙКЛНОПРСТУХЦЧЭЯ]/.test(ch))
+            return 8;
+        else if (/[Ww]/.test(ch))
+            return 10;
+        else if (/[MmмшщюМШЩЮ]/.test(ch))
+            return 11;
+        else if (ch == 'r' || ch == '-')
+            return 5;
+        else if (ch == ' ')
+            return 3;
+        else
+            return 7;
+    }
+    else if (fontSize == 24)
+    {
+        if (/[\(\)t]/.test(ch))
+            return 6;
+        else if (/[jiIJl1:;\.,]/.test(ch))
+            return 4;
+        else if (/[ABCDEFHGKLNOPQRSTUVXYZАБВГДЕЁЗИЙКЛНОПРСТУХЦЧЭЯ]/.test(ch))
+            return 9;
+        else if (/[wW]/.test(ch))
+            return 11;
+        else if (/[mMмшщюМШЩЮ]/.test(ch))
+            return 12;
+        else if (ch == 'r' || ch == '-')
+            return 5;
+        else if (ch == ' ')
+            return 4;
+        else
+            return 8;
+    }
+    else if (fontSize == 28)
+    {
+        if (/[\(\)t]/.test(ch))
+            return 8;
+        else if (/[jiIJl1:;\.,]/.test(ch))
+            return 5;
+        else if (/[ABCDEFHGKLNOPQRSTUVXYZАБВГДЕЁЗИЙКЛНОПРСТУХЦЧЭЯ]/.test(ch))
+            return 13;
+        else if (/[wW]/.test(ch))
+            return 14;
+        else if (/[mMмшщюМШЩЮ]/.test(ch))
+            return 15;
+        else if (ch == 'r' || ch == '-')
+            return 7;
+        else if (ch == ' ')
+            return 4;
+        else
+            return 11;
+    }
+    
+    console.log('Error in getCharWidth: fontSize = ' + fontSize + ', char = ' + ch);
+    return 8;
+}
+
+function strTruncate(string, fontSize, width) {
+    var len = 0;
+    var i = 0;
+    while (len <= width && i < string.length - 1) {
+        len += getCharWidth(string.substr(i, 1), fontSize);
+        i++;
+    }
+    if (len > width) {
+        if (len - width > 2)
+            i--;
+        var result = string.substr(0, i);
+        var wrappablePos = Math.max(result.lastIndexOf(' '), result.lastIndexOf('-'));
+        if (string.length > i && string.substr(i, 1) != ' ' && wrappablePos > 0)
+            result = string.substr(0, wrappablePos);
+
+        console.log('strTruncate: ' + result + ' -> ' + len + ' : ' + i);
         return result;
     }
     return string;
 }
 
-function strTruncateWhole(string, width) {
+function strTruncateWhole(string, fontSize, width) {
     var arr = [];
     var b = 0;
     while (b < string.length) {
-        arr.push(strTruncate(string.substring(b), width));
+        arr.push(strTruncate(string.substring(b), fontSize, width));
         b += arr[arr.length - 1].length;
     }
     return arr;
 }
 
-function sizeVector(string, fontSize, substractWidth) {
-    var width = 136 - (substractWidth || 0);
-    var charsPerLine;
-    if (fontSize==14)
-        charsPerLine = width / 5;
-    else if (fontSize==18)
-        charsPerLine = width / 6;
-    else if (fontSize==24)
-        charsPerLine = width / 7;
+function truncateAndCalculateSize(string, fontSize, substractWidth) {
+    var width = 144 - (substractWidth || 0);
     var lines = string.split('\n');
+    var rowsLeft = 30;
     var height = 0;
+    var result = '';
+    var restOfText = '';
     while (lines.length)
     {
-        var split = strTruncateWhole(lines.shift(), charsPerLine);
+        var paragraph = lines.shift();
+        var split = strTruncateWhole(paragraph, fontSize, width);
+        if (rowsLeft <= split.length)
+        {
+            result += split.slice(0,rowsLeft).join('');
+            restOfText = split.slice(rowsLeft).join('') + '\n' + lines.join('\n');
+            return [new Vector2(width, height + 3), result, restOfText];
+        }
+        
+        rowsLeft -= split.length;
         height += split.length * fontSize;
+        result += paragraph + '\n';
     }
-    console.log('return vector: ' + width + 'x' + height);
-    return new Vector2(width, height);
+
+    return [new Vector2(width, height + 3), string, ''];
+}
+
+function addTextToWindow(window, xPos, yPos, text, pageFontSize)
+{
+    do {
+        var result = truncateAndCalculateSize(text, pageFontSize, xPos);
+        var fragmentSize = result[0];
+        var fragmentText = result[1];
+        text = result[2];
+        var fragmentElement = new UI.Text({ position: new Vector2(xPos, yPos), size: fragmentSize, text: fragmentText, textAlign: 'left', textOverflow: 'wrap', color: 'black', font: 'gothic-' + pageFontSize });
+        window.add(fragmentElement);
+        console.log('--- text element added at ' + xPos + ':' + yPos + ' ---');
+        yPos += fragmentSize.y;
+    } while (text);
+    return yPos;
 }
